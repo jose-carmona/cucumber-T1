2a24fae6c02df07732a92a12d7a4caae7b219d0d
Jose Carmona <j.carmona.n@gmail.com>
2018-07-19 20:47:48 +0200

    tests cucumber + rest-assured sobre RickAndMortyAPI funcionado

--- cucumber-jvm-restassured/src/test/java/org/jose/bdd/Steps.java
+++ cucumber-jvm-restassured/src/test/java/org/jose/bdd/Steps.java
@@ -6,38 +6,77 @@ import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
 import cucumber.api.java.es.Cuando;
 
+import io.restassured.specification.RequestSpecification;
+import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class Steps {
 
+  RequestSpecification rs;
+  Response response;
 
-    @Before
-    public void setUp() {
-      baseURI = "https://rickandmortyapi.com";
-    }
+  @Before
+  public void setUp() {
+    baseURI = "https://rickandmortyapi.com";
+    rs = given();
+  }
 
-    @After
-    public void afterScenario() {
-    }
+  @After
+  public void afterScenario() {
+  }
 
-    @Dado("^que el ID del personaje es (\\d+)$")
-    public void que_el_ID_del_personaje_es(int arg1) throws Throwable {
-    }
+  @Dado("^que el ID del personaje es (\\d+)$")
+  public void que_el_ID_del_personaje_es(int Id) throws Throwable {
+    rs = rs.pathParam("Id", Id);
+  }
 
-    @Cuando("^invoco al API de personajes de Rick y Morty$")
-    public void invoco_al_API_de_personajes_de_Rick_y_Morty() throws Throwable {
-      given().
-        pathParam("Id", "1").
-      when().
-        get("/api/character/{Id}").
-      then().
-        assertThat().statusCode(200).
-        body("name", equalTo("Rick Sanchez"));
-    }
+  @Dado("^que el nombre del personaje a buscar es \"([^\"]*)\"$")
+  public void que_el_nombre_del_personaje_a_buscar_es(String name) throws Throwable {
+    rs = rs.param("name", name);
+  }
 
-    @Entonces("^el API de personajes de Rick y Morty debe mostrar el nombre 'Rick Sanchez'$")
-    public void el_API_de_personajes_de_Rick_y_Morty_debe_mostrar_el_nombre_Rick_Sanchez() throws Throwable {
-    }
+  @Dado("^que el estado del personaje a buscar es \"([^\"]*)\"$")
+  public void que_el_estado_del_personaje_a_buscar_es(String status) throws Throwable {
+    rs = rs.param("status", status);
+  }
+
+  @Cuando("^invoco al API de personajes de Rick y Morty por Id$")
+  public void invoco_al_API_de_personajes_de_Rick_y_Morty_Id() throws Throwable {
+    response = rs.when().get("/api/character/{Id}");
+    response.then().assertThat().statusCode(200);
+  }
+
+  @Cuando("^invoco al API de personajes de Rick y Morty filtrando por nombre$")
+  public void invoco_al_API_de_personajes_de_Rick_y_Morty_filtrando_por_nombre() throws Throwable {
+    response = rs.when().get("/api/character");
+  }
+
+  @Entonces("^el API debe devolver un statusCode (\\d+)$")
+  public void el_API_debe_devolver_un_statusCode(int code) throws Throwable {
+    response.then().assertThat().statusCode(code);
+  }
+
+  @Entonces("^el API de personajes de Rick y Morty debe mostrar el nombre \"([^\"]*)\"$")
+  public void el_API_de_personajes_de_Rick_y_Morty_debe_mostrar_el_nombre(String name) throws Throwable {
+    response.then().body("name", equalTo(name));
+  }
+
+  @Entonces("^el API de personajes de Rick y Morty debe mostrar la especia \"([^\"]*)\"$")
+  public void el_API_de_personajes_de_Rick_y_Morty_debe_mostrar_la_especia(String specie) throws Throwable {
+    response.then().body("species", equalTo(specie));
+  }
+
+  @Entonces("^el resultado debe contener (\\d+) personajes$")
+  public void el_resultado_debe_contener_personajes(int n) throws Throwable {
+    response.then().body("info.count", equalTo(n));
+  }
+
+  @Entonces("^el API debe arrojar el error \"([^\"]*)\"$")
+  public void el_API_debe_arrojar_el_error(String error) throws Throwable {
+    response.then().body("error", equalTo(error));
+  }
+
 }
--- cucumber-jvm-restassured/src/test/resources/cucumber/app_es.feature
+++ cucumber-jvm-restassured/src/test/resources/cucumber/app_es.feature
@@ -2,9 +2,37 @@
 Característica: Uso de cucumber para testear un APIs
   Como Friki
   Quiero acceder al API de Rick and Morty
-  Para hacer que el mundo sea un poco mejor
+  Para hacer que el mundo del desarrollo de software sea un poco mejor
 
-  Escenario: La API debe mostrar información sobre Rick Sanchez
+  Escenario: La API debe mostrar información sobre Rick Sanchez accediendo por el Id
     Dado que el ID del personaje es 1
-    Cuando invoco al API de personajes de Rick y Morty
-    Entonces el API de personajes de Rick y Morty debe mostrar el nombre 'Rick Sanchez'
+    Cuando invoco al API de personajes de Rick y Morty por Id
+    Entonces el API debe devolver un statusCode 200
+      Y el API de personajes de Rick y Morty debe mostrar el nombre "Rick Sanchez"
+      Y el API de personajes de Rick y Morty debe mostrar la especia "Human"
+
+  Escenario: La API debe mostrar información sobre Morty Smith accediendo por el Id
+    Dado que el ID del personaje es 2
+    Cuando invoco al API de personajes de Rick y Morty por Id
+    Entonces el API debe devolver un statusCode 200
+      Y el API de personajes de Rick y Morty debe mostrar el nombre "Morty Smith"
+      Y el API de personajes de Rick y Morty debe mostrar la especia "Human"
+
+  Escenario: La API debe permitirme localizar personajes por nombre
+    Dado que el nombre del personaje a buscar es "Rick"
+    Cuando invoco al API de personajes de Rick y Morty filtrando por nombre
+    Entonces el API debe devolver un statusCode 200
+      Y el resultado debe contener 73 personajes
+
+  Escenario: La API no debe mostrar ofrecer resultado cuando el nombre no exista
+    Dado que el nombre del personaje a buscar es "Nombrequenoexiste"
+    Cuando invoco al API de personajes de Rick y Morty filtrando por nombre
+    Entonces el API debe devolver un statusCode 404
+      Y el API debe arrojar el error "There is nothing here"
+
+  Escenario: La API debe permitirme localizar personajes por nombre y estado
+    Dado que el nombre del personaje a buscar es "Rick"
+      Y que el estado del personaje a buscar es "alive"
+    Cuando invoco al API de personajes de Rick y Morty filtrando por nombre
+    Entonces el API debe devolver un statusCode 200
+      Y el resultado debe contener 21 personajes
